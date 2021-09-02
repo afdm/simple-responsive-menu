882b4605c5dc693e1c761065a0dab5e11a15e365
Alix FDM <bonjour@alixfdm.fr>
2021-09-02 11:16:34 -0300

    add the Fade In animation on the open menu

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,19 @@
 const burger = document.querySelector(".burger")
 const headerMenu = document.querySelector(".header .menu")
+const headerMenuItems = document.querySelectorAll(".header .menu .item")
 
 burger.addEventListener("click", () => {
     headerMenu.classList.toggle("open")
     burger.classList.toggle("open")
+    
+    headerMenuItems.forEach((item, index) => {
+        if(item.style.animationDelay)
+            return item.style.animationDelay = "";
+           
+        const maxDelay = 0.8
+        const delay = index / 12
+        const animationDelay = delay >= maxDelay ? maxDelay : delay
+        
+        return item.style.animationDelay = `${animationDelay}s`        
+    })
 })
